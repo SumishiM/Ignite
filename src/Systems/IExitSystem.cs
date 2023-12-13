b140c87714c55247e39226b098697d907dbddebf
Hugo Michel <92516108+SumishiM@users.noreply.github.com>
2023-12-13 11:52:11 +0100

    Systems update

--- src/Systems/IExitSystem.cs
+++ src/Systems/IExitSystem.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Ignite.src.Systems
+﻿namespace Ignite.Systems
 {
-    internal interface IExitSystem
+    public interface IExitSystem : ISystem
     {
     }
 }
--- src/Systems/IRenderSystem.cs
+++ src/Systems/IRenderSystem.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Ignite.src.Systems
+﻿namespace Ignite.Systems
 {
-    internal interface IRenderSystem
+    public interface IRenderSystem : ISystem
     {
     }
 }
