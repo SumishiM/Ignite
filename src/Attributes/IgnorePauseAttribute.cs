d6d46f422695953d7dd6726e6013e610c45398fb
Hugo Michel <92516108+SumishiM@users.noreply.github.com>
2023-12-13 16:53:11 +0100

    Create IgnorePauseAttribute.cs

---
+++ src/Attributes/IgnorePauseAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ignite.Attributes
+{
+    /// <summary>
+    /// Indicate that a system will not be deactivated on pause
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = true)]
+    public class IgnorePauseAttribute : Attribute
+    {
+    }
+}
