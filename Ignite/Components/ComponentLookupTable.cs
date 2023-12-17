58e1d9b45aba82408f13d3110438ed051eec43c9
Hugo Michel <92516108+SumishiM@users.noreply.github.com>
2023-12-17 18:51:44 +0100

    Add Node components methods

--- Ignite/Components/ComponentLookupTable.cs
+++ Ignite/Components/ComponentLookupTable.cs
@@ -5,14 +5,41 @@ namespace Ignite.Components
 {
     public class ComponentLookupTable
     {
-        private readonly ImmutableDictionary<Type, int> _componentsIndex = 
-            new Dictionary<Type, int>().ToImmutableDictionary();
+        private readonly ImmutableDictionary<Type, int> _componentsIndex =
+            new Dictionary<Type, int>()
+            {
+                { typeof(IComponent), 0 } 
+            }
+            .ToImmutableDictionary();
 
+        /// <summary>
+        /// Get the index of the component of <see cref="Type"/> <paramref name="type"/>.
+        /// See <see cref="Ignite.Components.ComponentLookupTable.GetIndex(Type)"/>
+        /// </summary>
+        /// <param name="type"><see cref="Type"/> of the component we want the index of</param>
+        /// <returns>Index of the component</returns>
         public int this[Type type] => GetIndex(type);
 
+        /// <summary>
+        /// Get the index of the <paramref name="component"/>.
+        /// See <see cref="Ignite.Components.ComponentLookupTable.GetIndex(Type)"/>
+        /// </summary>
+        /// <returns>Index of the <paramref name="component"/></returns>
+        public int this[IComponent component] => GetIndex(component.GetType());
+
+        /// <summary>
+        /// Get the index of the component of <see cref="Type"/> <typeparamref name="T"/>
+        /// </summary>
+        /// <typeparam name="T"><see cref="Type"/> of the component we want the index of</typeparam>
+        /// <returns>Index of the component</returns>
         public int GetIndex<T>() where T : class, IComponent
             => GetIndex(typeof(T));
 
+        /// <summary>
+        /// Get the index of the component of <see cref="Type"/> <paramref name="type"/>
+        /// </summary>
+        /// <param name="type"><see cref="Type"/> of the component we want the index of</param>
+        /// <returns>Index of the component</returns>
         public int GetIndex(Type type)
         {
             Debug.Assert(typeof(IComponent).IsAssignableFrom(type),
@@ -25,9 +52,22 @@ namespace Ignite.Components
             return _componentsIndex[type];
         }
 
+        /// <summary>
+        /// Get the index of the component of <see cref="Type"/> <typeparamref name="T"/>
+        /// or create an index for it
+        /// </summary>
+        /// <typeparam name="T"><see cref="Type"/> of the component we wand the index of</typeparam>
+        /// <returns>Index of the component</returns>
         public int GetOrCreateIndex<T>() where T : class, IComponent
             => GetOrCreateIndex(typeof(T));
 
+
+        /// <summary>
+        /// Get the index of the component of <see cref="Type"/> <paramref name="type"/>
+        /// or create an index for it
+        /// </summary>
+        /// <param name="type"><see cref="Type"/> of the component we wand the index of</param>
+        /// <returns>Index of the component</returns>
         public int GetOrCreateIndex(Type type)
         {
             Debug.Assert(typeof(IComponent).IsAssignableFrom(type),
--- Ignite/Node.cs
+++ Ignite/Node.cs
@@ -1,30 +1,32 @@
 ﻿using Ignite.Components;
 using System.Diagnostics;
 using System.Diagnostics.CodeAnalysis;
+using System.Text;
 
 namespace Ignite
 {
     public abstract class Node
     {
-        public Dictionary<int, IComponent> Components { get; protected set; } = 
+        public Dictionary<int, IComponent> Components { get; protected set; } =
             new Dictionary<int, IComponent>();
 
         private readonly int Id;
         private readonly ComponentLookupTable _lookup;
 
-        public Node(World world)
+        public Node ( World world )
         {
             _lookup = world.Lookup;
         }
 
-        public Node(World world, params IComponent[] components)
+        public Node ( World world, params IComponent[] components )
             : this(world)
         {
-            foreach (var component in components)
+            foreach ( var component in components )
             {
                 Components.Add(_lookup[component.GetType()], component);
             }
         }
+
         /*
         public Node(World world, params Type[] components)
             : this(world)
@@ -35,23 +37,28 @@ namespace Ignite
                 //Components.Add(_lookup[component], component);
             }
         }
-         */
+        */
 
-        public bool HasComponent<T>() where T : class, IComponent
+        public bool HasComponent<T> () where T : class, IComponent
             => HasComponent(_lookup[typeof(T)]);
 
-        public bool HasComponent(Type type)
+        public bool HasComponent ( Type type )
             => HasComponent(_lookup[type]);
 
-        internal bool HasComponent(int index)
+        internal bool HasComponent ( int index )
         {
             return Components.ContainsKey(index);
         }
 
-        public bool TryGetComponent<T>([NotNullWhen(true)] out T? component) 
+        internal bool HasComponent ( IComponent component)
+        {
+            return Components.ContainsKey(_lookup[component]);
+        }
+
+        public bool TryGetComponent<T> ( [NotNullWhen(true)] out T? component )
             where T : class, IComponent
         {
-            if (TryGetComponent(typeof(T), out IComponent? result))
+            if ( TryGetComponent(typeof(T), out IComponent? result) )
             {
                 component = (T)result;
                 return true;
@@ -61,28 +68,28 @@ namespace Ignite
             return false;
         }
 
-        public bool TryGetComponent(Type type, [NotNullWhen(true)] out IComponent? component)
+        public bool TryGetComponent ( Type type, [NotNullWhen(true)] out IComponent? component )
         {
             Debug.Assert(typeof(IComponent).IsAssignableFrom(type),
                 $"Why are we trying to get and object that isn't a component!?");
 
-            if (Components.TryGetValue(_lookup[type], out IComponent? value))
+            if ( Components.TryGetValue(_lookup[type], out IComponent? value) )
             {
                 component = value;
                 return true;
             }
 
-            component = null; 
+            component = null;
             return false;
         }
 
-        public T GetComponent<T>() where T : class, IComponent
+        public T GetComponent<T> () where T : class, IComponent
         {
             Debug.Assert(HasComponent<T>(), $"This node doesn't contain this component");
             return (T)Components[_lookup[typeof(T)]];
         }
 
-        public IComponent GetComponent(Type type)
+        public IComponent GetComponent ( Type type )
         {
             Debug.Assert(typeof(IComponent).IsAssignableFrom(type),
                 $"Why are we trying to get and object that isn't a component!?");
@@ -90,14 +97,64 @@ namespace Ignite
             return Components[_lookup[type]];
         }
 
-        internal int GetComponentIndex<T>()
+        internal int GetComponentIndex<T> ()
         {
             return _lookup[typeof(T)];
         }
 
-        internal int GetComponentIndex(Type type)
+        internal int GetComponentIndex ( Type type )
         {
             return _lookup[type];
         }
+
+        public Node AddComponent<T> () where T : class, IComponent, new()
+            => AddComponent(new T());
+
+        public Node AddComponent ( Type type )
+        {
+            Debug.Assert(typeof(IComponent).IsAssignableFrom(type),
+                $"Why are we trying to add/replace a component with a type that isn't a component ?");
+
+            return AddComponent((IComponent)Activator.CreateInstance(type)!);
+        }
+
+        public Node AddComponent<T> ( T component ) where T : class, IComponent
+        {
+            Debug.Assert(!HasComponent(component),
+                $"Why are we adding a component to a node already having one of the same type ? Try AddOrReplaceComponent instead.");
+            
+            Components[_lookup[component.GetType()]] = component;
+            return this;
+        }
+
+        public Node AddOrReplaceComponent<T> () where T : class, IComponent, new()
+            => AddOrReplaceComponent(new T());
+
+        public Node AddOrReplaceComponent ( Type type )
+        {
+            Debug.Assert(typeof(IComponent).IsAssignableFrom(type),
+                $"Why are we trying to add/replace a component with a type that isn't a component ?");
+
+            return AddOrReplaceComponent((IComponent)Activator.CreateInstance(type)!);
+        }
+
+        public Node AddOrReplaceComponent<T> ( T component ) where T : class, IComponent
+        {
+            Components[_lookup[component.GetType()]] = component;
+            return this;
+        }
+
+        public Node RemoveComponent<T>()
+        {
+            Components.Remove(_lookup[typeof(T)]);
+            return this;
+        }
+
+        public Node RemoveComponent(Type type)
+        {
+            Components.Remove(_lookup[type]);
+            return this;
+        }
+
     }
 }
