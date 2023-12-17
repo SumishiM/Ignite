b7d1c1417ca3bd734f3b1c0a052d0873a641e969
Hugo Michel <92516108+SumishiM@users.noreply.github.com>
2023-12-17 21:11:45 +0100

    Split node class in different files

---
+++ Ignite/Node_Families.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Immutable;
+
+namespace Ignite
+{
+    public partial class Node
+    {
+        protected Node? _parent;
+        public Node? Parent => _parent;
+
+
+        private Dictionary<int,  Node> _children;
+        internal Dictionary<int, Node> ChildrenIndex => _children;
+        public ImmutableArray<Node> Children => _children.Values.ToImmutableArray();
+    }
+}
