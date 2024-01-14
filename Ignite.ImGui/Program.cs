8e391d5a7e125a40b3fa1377c138230c2b0a211a
Hugo Michel <92516108+SumishiM@users.noreply.github.com>
2024-01-14 22:28:14 +0100

    Start ImGui window

---
+++ Ignite.ImGui/Program.cs
@@ -0,0 +1,23 @@
+﻿using ClickableTransparentOverlay;
+using ImGuiNET;
+using System.Numerics;
+
+namespace Ignite.UI
+{
+    internal class Program : Overlay
+    {
+        static void Main ( string[] args )
+        {
+            Console.WriteLine("Hello, World!");
+            Program program = new();
+            program.Start().Wait();
+        }
+
+        protected override void Render ()
+        {
+            ImGui.Begin("Ignite");
+            ImGui.SetWindowSize("Ignite", new Vector2(1080, 720));
+            ImGui.End();
+        }
+    }
+}
