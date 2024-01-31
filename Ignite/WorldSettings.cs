1b2c80c1dd7e2c98747c40d3fbddcc9585b0f789
Hugo Michel <92516108+SumishiM@users.noreply.github.com>
2024-01-31 17:50:33 +0100

    Create world settings

---
+++ Ignite/WorldSettings.cs
@@ -0,0 +1,15 @@
+﻿namespace Ignite
+{
+    public class WorldSettings
+    {
+        /// <summary>
+        /// List of systems for the world
+        /// </summary>
+        public Type[] Systems = [];
+
+        /// <summary>
+        /// Nodes infos for nodes by default in the world with a name and a list of components
+        /// </summary>
+        public (string, Type[])[] DefaultNodes = [];
+    }
+}
