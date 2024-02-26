8e79b6609f5e1b80eb0a75c2d08b1f363ec38356
Hugo Michel <92516108+SumishiM@users.noreply.github.com>
2024-02-26 11:56:59 +0100

    Add extention method to get component from require component attribute from a component

---
+++ Ignite/Extentions/ComponentExtention.cs
@@ -0,0 +1,20 @@
+﻿using Ignite.Attributes;
+using Ignite.Components;
+using System.Reflection;
+
+namespace Ignite.Extentions
+{
+    public static class ComponentExtention
+    {
+        public static IComponent? GetFromRequirement(this IComponent component, Type type)
+        {
+            if (component.GetType().GetCustomAttribute(typeof(RequireComponentAttribute)) is RequireComponentAttribute attribute)
+            {
+                if (attribute.Components.TryGetValue(type, out var requiredComponent))
+                    return requiredComponent;
+            }
+
+            return null;
+        }
+    }
+}
