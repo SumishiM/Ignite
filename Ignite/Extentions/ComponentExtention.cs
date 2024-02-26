357fdc16db53652476c9a727480b4629a20bbb0d
Hugo Michel <92516108+SumishiM@users.noreply.github.com>
2024-02-26 12:01:01 +0100

    Add generic method for GetFromRequirement on component extention

--- Ignite/Extentions/ComponentExtention.cs
+++ Ignite/Extentions/ComponentExtention.cs
@@ -6,6 +6,12 @@ namespace Ignite.Extentions
 {
     public static class ComponentExtention
     {
+        /// <summary>
+        /// Get a <see cref="IComponent"/> from the <see cref="RequireComponentAttribute"/> if the component has this attribute
+        /// </summary>
+        /// <param name="component"></param>
+        /// <param name="type"></param>
+        /// <returns>The component of <see cref="Type"/> <paramref name="type"/> or null</returns>
         public static IComponent? GetFromRequirement(this IComponent component, Type type)
         {
             if (component.GetType().GetCustomAttribute(typeof(RequireComponentAttribute)) is RequireComponentAttribute attribute)
@@ -16,5 +22,14 @@ namespace Ignite.Extentions
 
             return null;
         }
+
+        /// <summary>
+        /// Get a <see cref="IComponent"/> of <see cref="Type"/> <typeparamref name="T"/> from the <see cref="RequireComponentAttribute"/> if the component has this attribute
+        /// </summary>
+        /// <param name="component"></param>
+        /// <returns>The component of <see cref="Type"/> <typeparamref name="T"/> or null</returns>
+        public static T? GetFromRequirement<T>(this IComponent component) where T : IComponent
+            => (T?)component.GetFromRequirement(typeof(T));
+        
     }
 }
